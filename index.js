1ecf03c402b59db957f24f636cadac319d6b1199
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-04-02 15:02:20 -0300

    newgrade route created and working

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import gradesRouter from './routes/grades.js';
-
-const port = 3000;
+import { promises as fs } from "fs";
+const {readFile, writeFile} = fs;
 
 const app = express();
 app.use(express.json());
 
+const port = 3000;
+global.__filename = 'grades.json';
+
 app.use("/grades", gradesRouter)
 
 app.listen(port, async () => {
-    console.log("listening at port number: " + port);
+    try {
+        await readFile(global.__filename);
+        console.log("Server is running...");
+    } catch (err) {
+        const initialJson = {
+            nextId: 0,
+            grades: []
+        }
+        writeFile(global.__filename, JSON.stringify(initialJson)).then(() => {
+            console.log("Server is running and the file was created");
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 });
--- routes/grades.js
+++ routes/grades.js
@@ -1,27 +1,25 @@
 import express from "express";
-const router = express.Router();
 import { promises as fs } from 'fs';
+
 const { writeFile, readFile } = fs;
 
-//const fileRead = readFile("./grades.json");
+const router = express.Router();
 
-function newGradeObject(student, subject, type, value) {
-  return new Object
-  ({
-      id: 500,
-      student: student,
-      subject: subject,
-      type: type,
-      value: value,
-      timestamp: new Date()
-  })
-}
+router.post("/newgrade", async (req, res) => {
+  try {
+    let grade = req.body;
+    //Lê o arquivo json com as notas e faz o parse para JSON
+    const data = JSON.parse(await readFile(global.__filename));
+    //cria o novo objeto a ser inserido, onde id é baseado no dado de dentro do arquivo lido acima
+    grade = { id: data.nextId++, ...grade, timestamp: new Date()};
+    data.grades.push(grade);
+    
+    await writeFile(global.__filename, JSON.stringify(data));
 
-router.post("/newgrade", (req, res) => {
-  const { student, subject, type, value } = req.body;
-  newGradeObject();
-  console.log(newGradeObject(student, subject, type, value));
-  res.end();
-})
+    res.send(grade);
+  } catch (err) {
+    res.status(400).send({error: err.message});
+  }
+});
 
 export default router;
